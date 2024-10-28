afcd0d73b20b11ed0ad131292487e6d2dcd5c0ac
4canzz <4canzz@gmail.com>
2024-10-28 18:41:17 +0500

    completed problem set problem 1 solution

--- Problem-Set-2/scrabble.c
+++ Problem-Set-2/scrabble.c
@@ -3,9 +3,50 @@
 #include <stdio.h>
 #include <ctype.h> 
 
-int main (void)
+int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+
+int calculate_score(string word);
+
+int main(void)
 {
+    string word_1 = get_string("Player 1: ");
+    string word_2 = get_string("Player 2: ");
+
+    int score_1 = calculate_score(word_1);
+    int score_2 = calculate_score(word_2);
+
+    if (score_1 > score_2)
+    {
+        printf("Player 1 wins!\n");
+    }
+    else if (score_1 < score_2)
+    {
+        printf("Player 2 wins!\n");
+    }
+    else
+    {
+        printf("Tie!\n");
+    }
     return 0;
+}
+
+int calculate_score(string word)
+{
+    int score = 0;
+
+    for (int i = 0, length = strlen(word); i < length; i++)
+    {
+        if (isupper(word[i]))
+        {
+            score += POINTS[word[i] - 'A'];
+        }
+        else if (islower(word[i]))
+        {
+            score += POINTS[word[i] - 'a'];
+        }
+    }
+
+    return score;
 }
 
 
