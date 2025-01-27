4db2aa8e64dccc7c4aac182df6fba70c7605d291
4canzz <4canzz@gmail.com>
2025-01-27 23:19:27 +0500

    added solution for mario-more

--- Problem-Set-1/mario-more.c
+++ Problem-Set-1/mario-more.c
@@ -1,3 +1,38 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int height;
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while (height < 1);
+
+    for (int i = 1; i <= height; i++)
+    {
+        for (int j = height; j > i; j--)
+        {
+            printf(" ");
+        }
+
+        for (int k = 0; k < i; k++)
+        {
+            printf("#");
+        }
+
+        printf("  ");
+
+        for (int l = 0; l < i; l++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
 /*Toward the beginning of World 1-1 in Nintendo’s Super Mario Brothers, Mario must hop over adjacent pyramids of blocks, per the below.
 
 screenshot of Mario jumping over adjacent pyramids
